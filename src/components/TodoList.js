9b6652670f2d5882776e9d650629a2b83fa79efe
choiseona <tjsdk617@naver.com>
2023-06-30 12:23:00 +0900

    Refactor: react-virualized
    optimize TodoList.js and TodoListItem.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,36 @@
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
+import React, { useCallback } from "react";
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(({ index, key, style }) => {
+    const todo = todos[index];
+    return (
+      <TodoListItem
+        todo={todo}
+        key={key}
+        onRemove={onRemove}
+        onToggle={onToggle}
+        style={style}
+      />
+    )
+  }, [onRemove, onToggle, todos])
+
   return (
-    <div className="TodoList">
-      {todos.map(todo => (
-        //map을 사용하여 컴포넌트로 변환할 때는 key props를 전달해 줘야 함
-        //여러 종류의 값을 전달해야 할 때는 객체 통재로 전달하는게 성능 최적화할 때 편리함
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-      ))}
-    </div>
+    <List
+      className="TodoList"
+      width={512} //전체 크기
+      height={513} //전체 높이
+      rowCount={todos.length} //항목 개수
+      rowHeight={57} //항목 높이
+      rowRenderer={rowRenderer} //항목을 렌더링할 때 쓰는 함수
+      list={todos} //배열
+      style={{ outline: 'none' }} //List에 기본 적용되는 outline 스타일 제거
+
+
+    />
   )
 }
 
-export default TodoList;
+export default React.memo(TodoList);
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,18 +7,20 @@ import {
 import cn from 'classnames';
 import React from 'react';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { text, checked, id } = todo;
 
   return (
-    <div className='TodoListItem'>
-      {/* cn: 인자로 들어간 값이 true면 class로 넣어주고 false면 안 넣어줌*/}
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className='text'>{text}</div>
-      </div>
-      <div className='remove' onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className='TodoListItem-virtualized' style={style}>
+      <div className='TodoListItem'>
+        {/* cn: 인자로 들어간 값이 true면 class로 넣어주고 false면 안 넣어줌*/}
+        <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className='text'>{text}</div>
+        </div>
+        <div className='remove' onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   )
